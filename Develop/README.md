906796d93b572e3a47355744a3b8e3f632cd723e
penaone <penaone@gmail.com>
2020-09-27 18:47:39 -0700

    Final other than badges

--- Develop/README.md
+++ Develop/README.md
@@ -1,24 +1,16 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `
-If (${data.license}=ISC) {
+If (APACHE-2.0=ISC) {
   [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC);  
-} else if (${data.license}=MIT) {
+} else if (APACHE-2.0=MIT) {
   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT);
-} else if (${data.license}=Apache-2.0) {
+} else if (APACHE-2.0=Apache-2.0) {
   [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0);
 } else {
   '';
 }
 
 
-# ${data.title}
+# headaceh
 
 
 ## Table of Contents
@@ -32,34 +24,30 @@ If (${data.license}=ISC) {
 
 
 ## Description:
-${data.description}
+asddddd
 
 
 ## Installation:
-${data.installation}
+kkkkkkkddddd
 
 
 ## Usage-Information:
-${data.usage}
+undefined
 
 
 ## License:
-This Repository is covered under the following license: [${data.license}] 
+This Repository is covered under the following license: [APACHE-2.0] 
 
 ## Contributors:
-${data.contributors}
+Sep
 
 
 
 ## Tests:
-${data.test}
+undefined
 
 
 ## Questions
 Contact me:
-Github: [https://github.com/${data.username}](https://github.com/${data.username})
-Email: [${data.email}](${data.email})
-`;
-}
-
-module.exports = generateMarkdown;
+Github: [https://github.com/pain](https://github.com/pain)
+Email: [pain@github](pain@github)
--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,132 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-];
+
+
+// array of questions for user
+const questions = inquirer.prompt([{
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your Project? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your Project title');
+          return false;
+        }
+      }
+    },
+
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your Project. (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your project description.');
+          return false;
+        }
+      }
+    },
+
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide installation instructions if needed or None. (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter installation instructions or None.');
+          return false;
+        }
+      }
+    },
+
+    {
+      type: 'input',
+      name: 'usage-information',
+      message: 'Please provide usage information. (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please provide usage information.');
+          return false;
+        }
+      }
+    },
+
+    {
+      type: 'input',
+      name: 'contributors',
+      message: 'Who if anyone contributed to this Project? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please list any contributors.');
+          return false;
+        }
+      }
+    },
+
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Describe any test required'
+    },
+
+    {
+      type: "list",
+      name: "license",
+      message: "Select the License",
+      choices: [
+        "MIT",
+        "APACHE-2.0",
+        "ISC",
+        "None"
+      ]
+    },
+
+    {
+      type: "input",
+      name: "username",
+      message: "please enter your github username"
+    },
+
+    {
+      type: "input",
+      name: "email",
+      message: "please enter your github affiliated email"
+    },
+
+
+  ])
+
+
+
+;
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeFile(fileName, data) {
+  fs.writeFile(fileName, data,'utf8', (err) => {
+    if (err) 
+      throw err;
+    
+  });
 }
 
 // function to initialize program
 function init() {
+  questions.then(answers => {
+    const response = generateMarkdown(answers);
+    writeFile("./README.md", response);
+  })
 
 }
 
